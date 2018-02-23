940b8ca5e6ec5065f7fd0927d8e71719053bb061
Shok <ShokerGooD@gmail.com>
2018-02-23 22:56:09 +0200

    Begin

---
+++ ProductDelivery/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProductDelivery.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
