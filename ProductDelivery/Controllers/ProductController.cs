eacfcf227adb653d736cd026354d625fa1e63c97
Max <maksym.zakirov@nure.ua>
2018-03-12 21:51:35 +0200

    DimonLoh

---
+++ ProductDelivery/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DataAccessLayer.Entities;
+using DataAccessLayer.Repositories;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ProductDelivery.Controllers
+{
+    public class ProductController : Controller
+    {
+        [HttpGet]
+        public IActionResult OpenProducts()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ShowProductInfo()
+        {
+            return View("");
+        }
+
+        //[Authorize]
+        //[HttpPost]
+        //public int AddToCart(int id)
+        //{
+
+        //    return
+        //}
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
