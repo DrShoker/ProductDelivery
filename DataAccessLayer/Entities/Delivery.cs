56fe4e1fb423baa6ba50a5faea5f5d3b0691cfff
Shok <ShokerGooD@gmail.com>
2018-06-01 20:11:39 +0300

    Updated Server and DAL

--- DataAccessLayer/Entities/Delivery.cs
+++ DataAccessLayer/Entities/Delivery.cs
@@ -10,6 +10,10 @@ namespace DataAccessLayer.Entities
         public int Id { get; set; }
         public DateTime Date { get; set; }
         public DeliveryStatus Status { get; set; }
+        public Delivery()
+        {
+            Date = DateTime.Now;
+        }
 
         public int ClientId { get; set; }
         public Client Client { get; set; }
